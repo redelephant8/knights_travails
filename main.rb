90d9b90e88bf59634fc3a12f46b1662310d2a8e8
Orian <gavishorian@gmail.com>
2022-06-16 18:12:29 +0300

    Finished project and formatting + trail

--- main.rb
+++ main.rb
@@ -1,59 +1,72 @@
-require 'pry'
 class Node
-    attr_accessor :data, :child, :parent
+  attr_accessor :data, :parent
 
-    def initialize(data)
-        @data = data
-        @child = nil
-        @parent = nil
-    end
+  def initialize(data)
+    @data = data
+    @parent = nil
+  end
 end
 
 class Options
-    def initialize()
-        @possibilities = [[1, 2], [2, 1], [-1, 2], [1, -2], [-2, 1], [2, -1], [-1, -2], [-2, -1]]
-        @used = []
-    end
+  def initialize()
+    @possibilities = [[1, 2], [2, 1], [-1, 2], [1, -2], [-2, 1], [2, -1], [-1, -2], [-2, -1]]
+    @used = []
+  end
 
-    def nextMove(data, goal)
-        return goal if data.include?(goal)
-        temp = []
-        if data == [[4, 4]]
-            root = Node.new(data)
+  def nextMove(original, goal)
+    final = 0
+    return goal if original == goal
+    temp = []
+    root = Node.new(original)
+    data = [root]
+    data.each do |arr|
+      root = arr
+      arr = arr.data
+      temp = getNext(arr, root)
+      data.shift
+      temp.each do |dataValue|
+        data.push(dataValue)
+      end
+      data.each do |dataValue|
+        if dataValue.data == goal
+          final = dataValue
+          break
         end
-        queue = [data]
-        while queue 
-            data.each do |arr|
-                temp = getNext(arr)
-                data.shift
-                temp.each do |dataValue|
-                    data.push(dataValue)
-                end
-                if data.include?([4, 4])
-                    binding.pry
-                    queue = false
-                    break
-                end
-            end
-        end
-        puts "s"
+      end
     end
+    counter = 0
+    finalPath = [final.data]
+    while final.data != original
+      counter += 1
+      final = final.parent
+      finalPath << final.data
+    end
+    puts "You made it in #{counter} moves! Here's your path: "
+    puts ""
+    final = final.parent
+    finalPath.reverse!
+    finalPath.each do |i|
+      print i
+      puts ""
+    end
+  end
 
-    def getNext(arr)
-        temp = []
-        @possibilities.each do |num|
-            x = arr[0] + num[0]
-            y = arr[1] + num[1]
-            if @used.include?([x, y]) == false
-                temp.push([x, y])
-                @used.push([x, y])
-            end
-        end
+  def getNext(arr, root)
+    temp = []
+    @possibilities.each do |num|
+      x = arr[0] + num[0]
+      y = arr[1] + num[1]
+      test = Node.new([x, y])
+      if @used.include?(test.data) == false && x.between?(0, 7) && y.between?(0, 7)
+        test.parent = root
+        temp.push(test)
+        @used.push([x, y])
+      end
+    end
     return temp
-    end
-
+  end
 end
 
 knight = Options.new
 
-p knight.nextMove([[4, 4]], [1,1])
+knight.nextMove([0, 0], [7, 7])
